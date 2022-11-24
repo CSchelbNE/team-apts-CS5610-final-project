52b7183ee68598ff12118523e1caf17c9680306f
mac <mac@macs-air.mynetworksettings.com>
2022-11-23 22:27:40 -0500

    added collapseable functionality

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -11,6 +11,7 @@ function SearchBar() {
     const [newInput, setNewInput] = useState("");
     const [isInputChanged, setIsChanged] = useState(false);
     const [key, setKey] = useState("artist");
+    const [visibility, setVisibility] = useState("d-none p-0")
     const buttonArtistStyle = key === "artist" ? "primary" : "outline-dark";
     const buttonAlbumStyle = key === "album" ? "primary" : "outline-dark";
     useEffect(() => {
@@ -22,43 +23,49 @@ function SearchBar() {
                     case "artist":
                         setIsChanged(false);
                         dispatch(getArtistsThunk(newInput));
+                        setVisibility("d-block p-0")
                     case "album":
                         setIsChanged(false);
                         dispatch(getArtistsThunk(newInput));
 
                 }
             }
-        }}, 200);
+        }}, 150);
         return () => clearTimeout(delayedGetRequest)
     }, [newInput]);
     const query = useSelector(state =>
         state.discogs
     )
-    console.log(query.discogsQuery)
-
     return (
         <div className="row wd-art-nuvo mt-3 p-0" style={{border: "1px solid lightgrey"}} >
             <div className="p-0 position-relative">
-                <input className="form-control row-cols-3 shadow-none" style={{borderRadius: 0}} placeholder={"Search for new records here..."} value={newInput} onChange={(e) =>
+                <input onFocus={()=> setTimeout(()=> setVisibility("p-0 d-block"),100)} className="form-control row-cols-3 shadow-none" style={{borderRadius: 0}} placeholder={"Search for new records here..."} value={newInput} onChange={(e) =>
                 {
+                    if (e.target.value === ""){
+                        setVisibility("d-none");
+                        setNewInput(e.target.value);
+                        return;
+                    }
                     setNewInput(e.target.value)
                     setIsChanged(true)}}/>
                 <FontAwesomeIcon  icon={faSearch} className="me-3 position-absolute end-0 top-50 translate-middle-y"/>
             </div>
-            <div className={"d-flex flex-row p-0"} style={{borderBottom: "1px solid lightgrey"}}>
-                <Button variant={buttonArtistStyle} onClick={() => setKey("artist")} style={{borderRadius:0}}>Artist</Button>
-                <Button variant={buttonAlbumStyle} onClick={()=> setKey("album")} style={{borderRadius:0}}>Album</Button>
-            </div>
-            <div className="wd-search-scroll-div p-0">
-                {query.discogsQuery.map(e =>
-                    <Card style={{borderRadius: 0, height: "85px"}} key={uuid4()} className="border-1 d-flex flex-row row-cols-4 mb-1">
-                        <img style={{height: "75px"}} src={e.thumb}/>
-                            <div className="p-0 d-flex flex-column justify-content-center"
-                                 style={{height: "75px", width: "75%"}}>
-                                <div className="p-1" style={{width: "fit-content"}}>{e.title.split("-")[0]}</div>
-                                <div className="p-1">Artist</div>
-                            </div>
-                    </Card>)}
+            <div className={visibility}>
+                <div className={"d-flex flex-row p-0"} style={{borderBottom: "1px solid lightgrey"}}>
+                    <Button variant={buttonArtistStyle} onClick={() => setKey("artist")} style={{borderRadius:0}}>Artist</Button>
+                    <Button variant={buttonAlbumStyle} onClick={()=> setKey("album")} style={{borderRadius:0}}>Album</Button>
+                </div>
+                <div className="wd-search-scroll-div p-0">
+                    {query.discogsQuery.map(e =>
+                        <Card style={{borderRadius: 0, height: "85px"}} key={uuid4()} className="border-1 d-flex flex-row row-cols-4 mb-1">
+                            <img style={{height: "80px"}} src={e.thumb}/>
+                                <div className="p-0 d-flex flex-column justify-content-center"
+                                     style={{height: "80px", width: "75%"}}>
+                                    <div className="p-1" style={{width: "fit-content"}}>{e.title.split("-")[0]}</div>
+                                    <div className="p-1">Artist</div>
+                                </div>
+                        </Card>)}
+                </div>
             </div>
     </div>
 
