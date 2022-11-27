6d53f8c637a02a2cb9d52d1becf9229ecf3bde6a
mac <mac@macs-air.mynetworksettings.com>
2022-11-27 11:00:09 -0500

    pre-merge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import SearchBar from "./components/search-bar";
 
 function App() {
     return (
         <div className="container">
-            <div className="col-3">
-                <SearchBar className="col-3"/>
-            </div>
         </div>
     )
 }
