88759564170d27ff011661463419ebcf03fdeb8a
aravaraghu <97917143+aravaraghu@users.noreply.github.com>
2022-12-03 17:54:53 -0500

    Removed unncessary divs in profile component.

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -5,14 +5,8 @@ import ProfileComponent from "./profile-component";
 const ProfileScreen = () => {
     return (
         <div className="">
-            {/* <div className="col-3"> */}
-                {/* <div className="w-100"> */}
-                    <NavigationSidebar/>
-                {/* </div> */}
-            {/* </div> */}
-            {/* <div className="col-9"> */}
+                <NavigationSidebar/>
                 <ProfileComponent />
-            {/* </div> */}
         </div>
     );
 }
