c56d3ee4b7667333cd372aa34e5dcb3833c9437a
mac <mac@macs-Air.cable.rcn.com>
2022-12-02 21:24:18 -0500

    prior to deleting the artist/album filter

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,15 +1,16 @@
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect, useState} from "react";
-import {getArtistsThunk} from "../services/discogs-thunk";
+import {getAlbumsThunk, getArtistsThunk} from "../services/discogs-thunk";
 import {Card, Button} from "react-bootstrap";
 import {uuid4} from "uuid4"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faSearch} from "@fortawesome/free-solid-svg-icons/faSearch";
 
-function SearchBar() {
+function SearchBar(isHidden) {
     const dispatch = useDispatch();
     const [newInput, setNewInput] = useState("");
     const [isInputChanged, setIsChanged] = useState(false);
+    const [isButtonClicked, setButtonClicked] = useState(false);
     const [key, setKey] = useState("artist");
     const [visibility, setVisibility] = useState("d-none p-0")
     const buttonArtistStyle = key === "artist" ? "primary" : "outline-dark";
@@ -24,22 +25,43 @@ function SearchBar() {
                         setIsChanged(false);
                         dispatch(getArtistsThunk(newInput));
                         setVisibility("d-block p-0")
+                        break;
                     case "album":
                         setIsChanged(false);
-                        dispatch(getArtistsThunk(newInput));
-
+                        dispatch(getAlbumsThunk(newInput));
+                        setVisibility("d-block p-0")
+                        break;
                 }
             }
         }}, 150);
         return () => clearTimeout(delayedGetRequest)
     }, [newInput]);
+
+    useEffect(() => {
+        if (newInput!=="") {
+                switch (key) {
+                    case "artist":
+                        setIsChanged(false);
+                        dispatch(getArtistsThunk(newInput));
+                        setVisibility("d-block p-0")
+                        break;
+                    case "album":
+                        setIsChanged(false);
+                        dispatch(getAlbumsThunk(newInput));
+                        setVisibility("d-block p-0");
+                        break;
+                }
+            }
+    }, [key])
     const query = useSelector(state =>
         state.discogs
     )
     return (
-        <div className="row wd-art-nuvo mt-3 p-0" style={{border: "1px solid lightgrey"}} >
+        <div  className="row wd-art-nuvo mt-3 p-0" style={{border: "1px solid lightgrey"}} >
             <div className="p-0 position-relative">
-                <input onBlur={() => setVisibility("p-0 d-none")} onFocus={()=> setTimeout(()=> setVisibility("p-0 d-block"),100)} className="form-control row-cols-3 shadow-none" style={{borderRadius: 0}} placeholder={"Search for new records here..."} value={newInput} onChange={(e) =>
+                <input onFocus={()=> setTimeout(()=> setVisibility("p-0 d-block"),100)}
+                       className="form-control row-cols-3 shadow-none" style={{borderRadius: 0}}
+                       placeholder={"Search for new records here..."} value={newInput} onChange={(e) =>
                 {
                     if (e.target.value === ""){
                         setVisibility("d-none");
@@ -52,11 +74,17 @@ function SearchBar() {
             </div>
             <div className={visibility}>
                 <div className={"d-flex flex-row p-0"} style={{borderBottom: "1px solid lightgrey"}}>
-                    <Button variant={buttonArtistStyle} onClick={() => setKey("artist")} style={{borderRadius:0}}>Artist</Button>
-                    <Button variant={buttonAlbumStyle} onClick={()=> setKey("album")} style={{borderRadius:0}}>Album</Button>
+                    <Button variant={buttonArtistStyle} onClick={() => {
+                        setKey("artist");
+                        setButtonClicked(true);
+                    }} style={{borderRadius:0}}>Artist</Button>
+                    <Button variant={buttonAlbumStyle} onClick={()=> {
+                        setKey("album");
+                        setButtonClicked(true);
+                    }} style={{borderRadius:0}}>Album</Button>
                 </div>
                 <div className="wd-search-scroll-div p-0">
-                    {query.discogsQuery.map(e =>
+                    {key === "artist" ? query.discogsQuery.map(e =>
                         <Card style={{borderRadius: 0, height: "85px"}} key={uuid4()} className="border-1 d-flex flex-row row-cols-4 mb-1">
                             <img style={{height: "80px"}} src={e.thumb}/>
                                 <div className="p-0 d-flex flex-column justify-content-center"
@@ -64,7 +92,17 @@ function SearchBar() {
                                     <div className="p-1" style={{width: "fit-content"}}>{e.title.split("-")[0]}</div>
                                     <div className="p-1">Artist</div>
                                 </div>
-                        </Card>)}
+                        </Card>) :
+                        query.discogsAlbumQuery.map(e =>
+                        <Card style={{borderRadius: 0, height: "85px"}} key={uuid4()} className="border-1 d-flex flex-row row-cols-4 mb-1">
+                            <img style={{height: "80px"}} src={e.thumb}/>
+                                <div className="p-0 d-flex flex-column justify-content-center"
+                                     style={{height: "80px", width: "75%"}}>
+                                    <div className="p-1" style={{width: "fit-content"}}>{!e.title.split("-")[1] ? e.title.split("-")[0] : e.title.split("-")[1]}</div>
+                                    <div className="p-1">{!e.title.split("-")[1] ? "Unknown Artist" : e.title.split("-")[0]}</div>
+                                </div>
+                        </Card>)
+                    }
                 </div>
             </div>
     </div>
