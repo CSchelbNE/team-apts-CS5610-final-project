aa792fd5db9eacdb8127b071c5386bd31a1b30b0
Lucian Tisdale <tisdale.lucian@gmail.com>
2022-12-07 15:52:11 -0500

    changes to HomeScreen 12/7

--- src/home/home-screen.js
+++ src/home/home-screen.js
@@ -1,23 +1,41 @@
 import React from "react";
 import NavigationSidebar from "../navigation-sidebar/nav-bar";
-import SearchBar from "../components/search-bar";
+import "./index.css";
+import SuggestedComponent from "./suggested-component";
+
 
 const HomeScreen = () => {
     return (
         <div className="">
-            {/* <div className="col-3"> */}
-                {/* <div className="w-100"> */}
-                    <NavigationSidebar/>
-                {/* </div> */}
-                {/* <h3 className="w-100 bg-white mt-3">FollowingComponent</h3> */}
-            {/* </div> */}
-            {/* <div className="col-6"> */}
-                {/* <h1 className="bg-white">HomeScreen</h1> */}
-            {/* </div> */}
-            {/* <div className="col-3"> */}
-                {/* <SearchBar/> */}
-            {/* </div> */}
-            <h1>HomeScreen</h1>
+
+            <NavigationSidebar/>
+
+            <div className="container mt-2">
+                <div className="position-relative">
+                    <img src={"https://townsquare.media/site/295/files/2021/01/psych.jpg"} className="w-100 wd-banner-image-format"/>
+                    <div className="wd-title-format">
+                        Vintage Vinyl
+                    </div>
+                    <div className="wd-title2-format">
+                        Review Your Favorite Albums
+                    </div>
+                    <div className="wd-title3-format">
+                        Buy / Sell
+                    </div>
+                </div>
+                <div>
+                    <h1>Suggested Albums</h1>
+                    <div className="border border-2 border-secondary p-2 m-2">
+                        <SuggestedComponent/>
+                    </div>
+                </div>
+                <div>
+                    <h1>Recent Users</h1>
+                </div>
+                <div>
+                    <h1>Wish List (LoggedIn Content)</h1>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/home/suggested-array.js
@@ -0,0 +1,60 @@
+const suggestedArray = [
+{
+  "_id": "638ff7504053f1db576c7e1f",
+  "discogs_id": 75394,
+  "record_name": "Experience Hendrix (The Best Of Jimi Hendrix)",
+  "record_artist": "Jimi Hendrix",
+  "record_genre": "Rock",
+  "record_year": "1997",
+  "record_price": 5.99,
+  "record_quantity": 1,
+  "record_image": "https://i.discogs.com/gqBukIXFakpLi-2JHVzC6NUzZFkSHJKqYz3ljOoUMZg/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE1NDE0/NTItMTQ5NDUwMDc0/Mi04NzI3LmpwZWc.jpeg",
+  "record_vendor": "ben",
+  "__v": 0
+},
+{
+"_id": "638d1f881020b96a92d85d5b",
+"record_name": "In Utero",
+"record_artist": "Nirvana",
+"record_genre": "Rock",
+"record_year": 1990,
+"record_price": 19.99,
+"record_quantity": 2,
+"record_image": "https://i.discogs.com/qdlAyskWtIeV3BNVd6yf8mENh6b1G-BlEY00BDkm1_Y/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM3NTk3/OS0xNDkxNzAwMzQ3/LTcyNjMuanBlZw.jpeg",
+"record_vendor": 1234
+},
+{
+"_id": "638d15741020b96a92d85d5a",
+"record_name": "Rumours",
+"record_artist": "Fleetwood Mac",
+"record_genre": "Rock",
+"record_year": 1977,
+"record_price": 19.99,
+"record_quantity": 5,
+"record_image": "https://cdn.shopify.com/s/files/1/2976/0132/products/1813751_720x.jpg?…",
+"record_vendor": 123
+},
+  {"_id":
+    "638ff2f54053f1db576c7e1a",
+    "discogs_id":
+    65664,
+    "record_name":
+    "Miles Davis At Fillmore",
+    "record_artist":
+    "Miles Davis",
+    "record_genre":
+    "Jazz",
+    "record_year":
+    "1970",
+    "record_price":
+    20.99,
+    "record_quantity":
+    1,
+    "record_image":
+    "https://i.discogs.com/BqSdAJzTWx3F1ETUnOR5oPBSQ1WWYgtJ8LoGEXsfaTs/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc0MjYw/NzQtMTU0MTQ3NjIz/Mi04ODIyLmpwZWc.jpeg",
+    "record_vendor":
+    "ben",
+    "__v":
+    0},
+]
+export default suggestedArray;
