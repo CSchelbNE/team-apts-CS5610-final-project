7eadfdbb29df52275d4407f35c3ac7b1699449a5
salonipatil <patil.sal@northeastern.edu>
2022-12-09 04:54:07 -0500

    Listings

--- src/details/details-screen.js
+++ src/details/details-screen.js
@@ -11,14 +11,45 @@ const DetailsScreen = ({listing}) => {
     return (
         <>
         {!singleListing ? <></> :
-         <div>
-            <div className="d-inline-flex col-5 border border-2 border-primary">
-                <h1> Column 1</h1>
-                {/*<img src={singleListing[0].record_image}/>*/}
-            </div>
-            <div className="d-inline-flex col-5 border border-2 border-danger">
-                <h1>Column 2</h1>
-            </div>
+         <div className="container text-center">
+            <div className="row">
+                <div className="d-grid col-4 m-5">
+                    <div className="row">
+                        <div className="p-2">
+                            <img src={singleListing[0].record_image}/>
+                        </div>
+                        <div className="p-2">
+                            <button className="btn btn-outline-dark">Add to cart</button>
+                        </div>
+                        <div className="p-2">
+                            <button className="btn btn-outline-dark">Add to wishlist</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="d-grid col-5 m-2">
+                    <div className="row">
+                        <div className="p-2">
+                            <h2 className="text-dark">{singleListing[0].record_name}</h2>
+                        </div>
+                        <div className="p-2">
+                            <h3 className="text-dark">By: {singleListing[0].record_artist}</h3>
+                        </div>
+                        <div className="p-2">
+                            <h5 className="text-dark">Genre: {singleListing[0].record_genre}</h5>
+                        </div>
+                        <div className="p-2">
+                            <h5 className="text-dark">Year recorded: {singleListing[0].record_year}</h5>
+                        </div>
+                        <div className="p-2">
+                            <h5 className="text-dark">Qty available: {singleListing[0].record_quantity}</h5>
+                        </div>
+                        <div className="p-2">
+                            <h5 className="text-dark">Price: ${singleListing[0].record_price}</h5>
+                        </div>
+                    </div>
+                </div>
+         </div>
          </div>
             }
         </>
--- src/details/listing-array-component.js
+++ src/details/listing-array-component.js
@@ -3,15 +3,18 @@ import {useNavigate} from "react-router-dom";
 const ListingArrayComponent = ({listing}) => {
     const navigate = useNavigate();
     return (
-        <div onClick={() => {
-            navigate("/details/"+listing._id.toString());
-        }}>
-            <img src={listing.record_image}/>
-            <div>
-                {listing.record_artist + " : " + listing.record_name}
-            </div>
+
+        <div onClick ={() =>
+    {
+        navigate("/details/" + listing._id.toString());
+    }}>
+                <div className="text-center">
+                    <img src={listing.record_image} className="rounded"/>
+                    <h1 className="text-dark">{listing.record_name}</h1>
+                    <h3 className="text-dark">Artist: {listing.record_artist}</h3>
+                </div>
         </div>
-    )
-}
+    );
+};
 
 export default ListingArrayComponent;
