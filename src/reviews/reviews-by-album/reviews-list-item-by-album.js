52570dd96563ae512a7fcb9e6f08ac0bef3fb388
aravaraghu <97917143+aravaraghu@users.noreply.github.com>
2022-12-15 19:42:17 -0500

    Reviews UI updates

--- src/reviews/reviews-by-album/reviews-list-item-by-album.js
+++ src/reviews/reviews-by-album/reviews-list-item-by-album.js
@@ -16,57 +16,40 @@ const ReviewsListItemByAlbum = ({review, currentUser, profileUser}) => {
     return(
         <>
             {/*<Link to={"/details/" + review._id}>*/}
-                <li className="list-group-item">
-                    <div className="text-start">
-                        <div className="row ">
-                            {
-                                currentUser && profileUser && currentUser.username === profileUser.username &&
-                                <div className="text-end ">
-                                    <img src={bigXImg} className="wd-big-x-format" onClick={deleteReview}/>
-                                </div>
-                            }
 
-                            <div className="text-center">
-                                <Link to={"/details/" + review.listing._id + "?query=" + review.listing.record_name.split(' ')[0]}>
-                                    <div className="d-inline-flex">
-                                        <div className="">
-                                            <img src={review.listing.record_image} className="wd-thumb-format"/>
-                                        </div>
-                                        <div className="flex-inline flex-nowrap text-start ms-2 my-auto">
-                                            <div>{review.listing.record_name}</div>
-                                            <div>{review.listing.record_artist}</div>
-                                            <div>{review.listing.record_year}</div>
-                                        </div>
-                                    </div>
-                                </Link>
-                            </div>
+                <li className="list-group-item">
+                    <div className="d-flex">
+                        <div>
+                            <Link to={"/details/" + review.listing._id + "?query=" + review.listing.record_name.split(' ')[0]}>
+                                <img src={review.listing.record_image} className="rounded-3" width="70px" height="70px"/>
+                            </Link>
                         </div>
-                        <div className="row ">
-                            <div className="ms-0 col-2 ">
-                                <img src={review.user.profilePic}
-                                     className="wd-profile-pic-large-format rounded-circle mt-2"/>
-                            </div>
-                            <div className="col-11 col-lg-10 col-md-10 col-sm-11">
-                                <div className="float-end wd-nowrap">
-                                    {/*5 stars*/}
-                                    <img src={require(review.rating >= 1 ? "../../images/gold-star-icon.png" : "../../images/gray-star-icon.jpg")}
-                                         className="wd-gold-star-format me-1" />
-                                    <img src={require(review.rating >= 2 ? "../../images/gold-star-icon.png" : "../../images/gray-star-icon.jpg")}
-                                         className="wd-gold-star-format me-1" />
-                                    <img src={require(review.rating >= 3 ? "../../images/gold-star-icon.png" : "../../images/gray-star-icon.jpg")}
-                                         className="wd-gold-star-format me-1" />
-                                    <img src={require(review.rating >= 4 ? "../../images/gold-star-icon.png" : "../../images/gray-star-icon.jpg")}
-                                         className="wd-gold-star-format me-1" />
-                                    <img src={require(review.rating >= 5 ? "../../images/gold-star-icon.png" : "../../images/gray-star-icon.jpg")}
-                                         className="wd-gold-star-format me-1" />
+                        <div className="d-flex flex-column ms-3">
+                                <div className="d-flex">
+                                        <div className="flex-grow-1">
+                                            <img src={review.user.profilePic}className="rounded-pill" width="30px" height="30px"/>
+                                            <span className="ps-2">{review.user.username}</span>
+                                        </div>
+                                        <div>
+                                            <img src={bigXImg} className="wd-big-x-format" onClick={deleteReview}/>
+                                        </div>
                                 </div>
-                                <div className="float-start mt-2">
-                                    <div>
+                                    <div className="mt-2 d-flex">
+                                        <img src={require(review.rating >= 1 ? "../../images/gold-star-icon.png" : "../../images/gray-star-icon.jpg")}
+                                                className="wd-gold-star-format me-1" />
+                                        <img src={require(review.rating >= 2 ? "../../images/gold-star-icon.png" : "../../images/gray-star-icon.jpg")}
+                                                className="wd-gold-star-format me-1" />
+                                        <img src={require(review.rating >= 3 ? "../../images/gold-star-icon.png" : "../../images/gray-star-icon.jpg")}
+                                                className="wd-gold-star-format me-1" />
+                                        <img src={require(review.rating >= 4 ? "../../images/gold-star-icon.png" : "../../images/gray-star-icon.jpg")}
+                                                className="wd-gold-star-format me-1" />
+                                        <img src={require(review.rating >= 5 ? "../../images/gold-star-icon.png" : "../../images/gray-star-icon.jpg")}
+                                                className="wd-gold-star-format me-1" />
+                                    </div>
+                                    <div className="word-break-all d-flex">
                                         {review.body}
                                     </div>
-                                </div>
-                            </div>
-                        </div>
+                       </div>
                     </div>
                 </li>
             {/*</Link>*/}
