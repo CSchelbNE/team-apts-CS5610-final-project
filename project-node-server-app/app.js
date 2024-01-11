a7fcd875ae37d4ffa2659aca272f68ceb2bb5ed8
salonipatil <patil.sal@northeastern.edu>
2024-01-11 05:40:22 -0500

    Details

--- project-node-server-app/app.js
+++ project-node-server-app/app.js
@@ -4,8 +4,8 @@ import HelloController from "./controllers/hello-controller.js";
 import RecordController from "./controllers/record-controller.js";
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb"//localhost:27017/records')
-// mongodb+srv://APTS-final-project:<password>@cluster0.80l7cee.mongodb.net/?retryWrites=true&w=majority
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb"//localhost:27017/records'
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
 app.use(express.json())
